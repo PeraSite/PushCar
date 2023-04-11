7c096c9728109f786ff4388b7fbe5710177a1c3a
PeraSite <pretocki3@gmail.com>
2023-04-11 22:05:40 +0900

    chore: 접속 시도 시 Ping 패킷 보내는 기능 추가

--- Assets/Scripts/JoinUI.cs
+++ Assets/Scripts/JoinUI.cs
@@ -1,5 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Net;
+using TMPro;
+using UnityEngine;
 
 public class JoinUI : MonoBehaviour {
-	
+	[SerializeField] private TMP_InputField _ipInput;
+	[SerializeField] private TMP_InputField _portInput;
+
+	public void Join() {
+		if (!IPAddress.TryParse(_ipInput.text, out var ip)) {
+			Debug.LogError("IP is invalid");
+			return;
+		}
+
+		if (!int.TryParse(_portInput.text, out var port)) {
+			Debug.LogError("Port is invalid integer");
+			return;
+		}
+
+		var endpoint = new IPEndPoint(ip, port);
+		NetworkManager.Instance.Join(endpoint);
+	}
 }
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using PushCar.Common.Packets.Client;
+using PushCar.Common.Utils;
+
+public sealed class NetworkManager : IDisposable {
+	private TcpClient _client;
+	private NetworkStream _stream;
+	private BinaryReader _reader;
+	private BinaryWriter _writer;
+
+	// 현재 서버와 연결되어있는가?
+	private bool _connected;
+
+	// Singleton 패턴
+	private NetworkManager() {
+		// TCP 클라이언트 초기화
+		_client = new TcpClient();
+	}
+	private static NetworkManager _instance;
+	public static NetworkManager Instance => _instance ??= new NetworkManager();
+
+	public void Dispose() {
+		if (!_connected) return;
+		_client?.Dispose();
+		_stream?.Dispose();
+		_reader?.Dispose();
+		_writer?.Dispose();
+	}
+
+	public void Join(IPEndPoint endpoint) {
+		if (_connected) {
+
+			Debug.Log("Can't join twice!");
+			return;
+		}
+
+		Debug.Log($"Trying to join {endpoint}");
+		_client.Connect(endpoint);
+
+		_stream = _client.GetStream();
+		_writer = new BinaryWriter(_stream);
+		_reader = new BinaryReader(_stream);
+		_connected = true;
+		Debug.Log("Connected!");
+
+		_writer.Write(new ClientPingPacket());
+	}
+}
