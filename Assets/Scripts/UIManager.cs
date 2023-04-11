220785d3ff56b018de442f4212a06571555634f2
PeraSite <pretocki3@gmail.com>
2023-04-11 21:40:09 +0900

    feat: Init

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+	[SerializeField] private CarController _car;
+	[SerializeField] private Transform _flag;
+	[SerializeField] private GameObject _retryButton;
+
+	[SerializeField] private TextMeshProUGUI _distanceText;
+
+	private void Start() {
+		_retryButton.SetActive(false);
+	}
+
+	private void Update() {
+		UpdateUI();
+	}
+
+	private void UpdateUI() {
+		if (_car.CanMove) {
+			_distanceText.text = "Swipe to move!";
+			return;
+		}
+
+		var distance = _flag.position.x - _car.transform.position.x;
+		if (_car.Stopped) {
+			_retryButton.SetActive(true);
+			_distanceText.text = distance <= 0f ? "Game Over!" : $"Result: {distance:F2}m";
+			return;
+		}
+
+		_distanceText.text = $"Distance: {distance:F2}m";
+	}
+
+	public void Retry() {
+		SceneManager.LoadScene("Play");
+	}
+}
