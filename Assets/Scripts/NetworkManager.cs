0416ae64c8a1830af96950ad175b8f53be2817e7
PeraSite <pretocki3@gmail.com>
2023-04-13 20:09:01 +0900

    feat: 유저 서버 접속 기능 추가

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,51 +1,102 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using Cysharp.Threading.Tasks;
+using PushCar.Common;
 using PushCar.Common.Packets.Client;
-using PushCar.Common.Utils;
+using PushCar.Common.Extensions;
+using PushCar.Common.Packets.Server;
+using UnityEngine;
 
-public sealed class NetworkManager : IDisposable {
+public sealed class NetworkManager : MonoBehaviour {
+	public static NetworkManager Instance { get; private set; }
+
 	private TcpClient _client;
 	private NetworkStream _stream;
 	private BinaryReader _reader;
 	private BinaryWriter _writer;
+	private ConcurrentQueue<IPacket> _packetQueue;
 
-	// 현재 서버와 연결되어있는가?
-	private bool _connected;
-
-	// Singleton 패턴
-	private NetworkManager() {
-		// TCP 클라이언트 초기화
+	private void Awake() {
 		_client = new TcpClient();
+		Instance = this;
+		_packetQueue = new ConcurrentQueue<IPacket>();
 	}
-	private static NetworkManager _instance;
-	public static NetworkManager Instance => _instance ??= new NetworkManager();
 
-	public void Dispose() {
-		if (!_connected) return;
+	private void OnDestroy() {
+		if (!_client.Connected) return;
 		_client?.Dispose();
 		_stream?.Dispose();
 		_reader?.Dispose();
 		_writer?.Dispose();
 	}
 
-	public void Join(IPEndPoint endpoint) {
-		if (_connected) {
+	private void Update() {
+		if (!_packetQueue.TryDequeue(out var incomingPacket)) return;
 
+		Debug.Log($"Received packet: {incomingPacket}");
+		switch (incomingPacket) {
+			case ServerPongPacket packet: {
+				Debug.Log("Pong!");
+				break;
+			}
+			default: {
+				throw new ArgumentOutOfRangeException();
+			}
+		}
+	}
+
+	public void SendPacket(IPacket packet) {
+		_writer.Write(packet);
+	}
+
+	public async UniTask<bool> Join(IPEndPoint endpoint) {
+		if (_client.Connected) {
 			Debug.Log("Can't join twice!");
-			return;
+			return false;
 		}
 
 		Debug.Log($"Trying to join {endpoint}");
-		_client.Connect(endpoint);
+		try {
+			await _client.ConnectAsync(endpoint.Address, endpoint.Port);
+		}
+		catch (Exception e) {
+			Debug.Log($"Error : {e.Message}");
+			return false;
+		}
 
 		_stream = _client.GetStream();
 		_writer = new BinaryWriter(_stream);
 		_reader = new BinaryReader(_stream);
-		_connected = true;
 		Debug.Log("Connected!");
 
+		// Ping after connected
 		_writer.Write(new ClientPingPacket());
+
+		// Run packet listen thread
+		await UniTask.RunOnThreadPool(() => {
+			while (_client.Connected) {
+				if (!_stream.CanRead) continue;
+				if (!_stream.CanWrite) continue;
+				if (!_client.Connected) {
+					Debug.Log("Disconnected!");
+					break;
+				}
+
+				var packetID = _reader.BaseStream.ReadByte();
+
+				// 읽을 수 없다면(데이터가 끝났다면 리턴)
+				if (packetID == -1) break;
+
+				var packetType = (PacketType)packetID;
+
+				// 타입에 맞는 패킷 객체 생성 후 큐에 추가
+				var basePacket = packetType.CreatePacket(_reader);
+				_packetQueue.Enqueue(basePacket);
+			}
+		}, cancellationToken: this.GetCancellationTokenOnDestroy());
+		return true;
 	}
 }
---
+++ Assets/Scripts/UI/JoinUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI {
+	public class JoinUI : MonoBehaviour {
+		[SerializeField] private TMP_InputField _ipInput;
+		[SerializeField] private TMP_InputField _portInput;
+
+		public void Join() {
+			if (!IPAddress.TryParse(_ipInput.text, out var ip)) {
+				Debug.LogError("IP is invalid");
+				return;
+			}
+
+			if (!int.TryParse(_portInput.text, out var port)) {
+				Debug.LogError("Port is invalid integer");
+				return;
+			}
+
+			var endpoint = new IPEndPoint(ip, port);
+			UniTask.Void(async () => {
+				var result = await NetworkManager.Instance.Join(endpoint);
+				if (result) {
+					SceneManager.LoadScene("Login");
+				}
+			});
+		}
+	}
+}
