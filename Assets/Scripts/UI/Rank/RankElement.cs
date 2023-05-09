018c0cf1a4f658fd8fc572aa67d9e4e311e65bf1
PeraSite <pretocki3@gmail.com>
2023-05-09 09:22:30 +0900

    feat: 랭킹 페이지에 날짜 표기 추가

--- Assets/Scripts/UI/Rank/RankElement.cs
+++ Assets/Scripts/UI/Rank/RankElement.cs
@@ -8,6 +8,7 @@ namespace PushCar.UI.Rank {
 	public class RankElement : MonoBehaviour {
 		[SerializeField] private TextMeshProUGUI _rank;
 		[SerializeField] private TextMeshProUGUI _distance;
+		[SerializeField] private TextMeshProUGUI _date;
 		[SerializeField] private TextMeshProUGUI _id;
 		[SerializeField] private Button _replayButton;
 		[SerializeField] private ReplayCache _replayCache;
@@ -15,6 +16,7 @@ namespace PushCar.UI.Rank {
 		public void Init(int rank, Record record) {
 			_rank.text = $"{rank}";
 			_distance.text = $"{record.Distance:F2}m";
+			_date.text = $"{record.Time:yyyy-MM-dd HH:mm:ss}";
 			_id.text = record.Id;
 			_replayButton.onClick.AddListener(() => {
 				_replayCache.Record = record;
